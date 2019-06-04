678ccc303780edde89668964878fcab2f1aa7d64
Qiusheng <sheng.qiu@fa-software.com>
2019-06-04 22:48:27 +0800

    ............

--- angular-cdk/src/app/overlay/overlay.component.ts
+++ angular-cdk/src/app/overlay/overlay.component.ts
@@ -1,7 +1,8 @@
-import {Component, TemplateRef, ViewChild, ViewContainerRef, ViewEncapsulation} from '@angular/core';
+import { Component, ElementRef, TemplateRef, ViewChild, ViewContainerRef, ViewEncapsulation } from '@angular/core';
 import {Overlay, OverlayConfig, OverlayRef, PositionStrategy} from '@angular/cdk/overlay';
 import set = Reflect.set;
-import {TemplatePortal, TemplatePortalDirective} from "@angular/cdk/portal";
+import { ComponentPortal, TemplatePortal, TemplatePortalDirective } from '@angular/cdk/portal';
+import { PanelComponent } from './panel/panel.component';
 
 @Component({
   selector: 'overlay',
@@ -15,6 +16,7 @@ export class OverlayComponent  {
   top = 0;
   overlayRef: OverlayRef;
   @ViewChild('overlayGlobalTemplate') overlayGlobalTemplate: TemplateRef<any>;
+  @ViewChild('origin') origin: ElementRef<any>;
   constructor(public overlay: Overlay,
               private viewContainer: ViewContainerRef
     ) {
@@ -25,13 +27,16 @@ export class OverlayComponent  {
     const positionStrategy: PositionStrategy = this.overlay.position().global().centerHorizontally().top(`${this.top}px`);
     this.top += 30;
     const config = new OverlayConfig();
+    config.height='100px';
+    config.width='100px';
+    config.panelClass=['dasda','ewewe'];
     config.positionStrategy = positionStrategy;
     //config.hasBackdrop = true;
     this.overlayRef = this.overlay.create(config);
     console.log(this.overlayRef);
     const templatePortal = new TemplatePortal(this.overlayGlobalTemplate, this.viewContainer);
     if (this.overlayRef.hasAttached()) {
-      this.overlayRef.detach();
+      this.overlayRef.dispose();
     }
     this.overlayRef.attach(templatePortal);
 
@@ -40,5 +45,52 @@ export class OverlayComponent  {
     });*/
     console.log(this.overlayRef);
 
+  }
+
+
+  /**
+   * overlay 在整个屏幕位置，自己控制位置
+   */
+  showOverlayGlobalPanelPosition() {
+    const config = new OverlayConfig();
+    config.positionStrategy = this.overlay.position()
+                                  .global()
+                                  .left(`${this.top}px`)
+                                  .top(`${this.top}px`);
+    this.top += 30;
+    config.hasBackdrop = true;
+    const overlayRef = this.overlay.create(config);
+    overlayRef.backdropClick().subscribe(() => {
+      overlayRef.dispose();
+    });
+    overlayRef.attach(new ComponentPortal(PanelComponent, this.viewContainer));
+  }
+
+  dismissOverlayPanelTemplate() {
+    if (this.overlayRef && this.overlayRef.hasAttached()) {
+      this.overlayRef.dispose();
+    }
+  }
+
+
+  /**
+   * overlay connect origin 显示，依附某个组件显示
+   */
+  showOverlayPanelConnectComponent() {
+    const strategy = this.overlay.position()
+                         .flexibleConnectedTo(this.origin.nativeElement)
+                         .withPositions([{
+                           originX: 'center',
+                           originY: 'bottom',
+                           overlayX: 'center',
+                           overlayY: 'top',
+                           offsetX: 0,
+                           offsetY: 0
+                         }]);
+    strategy.withLockedPosition(true);
+    const config = new OverlayConfig({positionStrategy: strategy});
+    config.scrollStrategy = this.overlay.scrollStrategies.reposition();
+    this.overlayRef = this.overlay.create(config);
+    this.overlayRef.attach(new ComponentPortal(PanelComponent, this.viewContainer));
   }
 }
