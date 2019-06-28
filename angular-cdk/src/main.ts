d8b17d80a69092cec7010502603f95cb4aa2f2d0
qiusheng1 <qiusheng1@lenovo.com>
2019-06-28 17:25:52 +0800

    ，，，，，

--- angular-cdk/src/main.ts
+++ angular-cdk/src/main.ts
@@ -3,7 +3,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import 'echarts';
+import 'echarts/theme/dark.js';
+import 'echarts/theme/macarons.js';
+import './assets/map/js/world.js';
 
 if (environment.production) {
   enableProdMode();
