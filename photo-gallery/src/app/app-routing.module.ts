0842a2b9dd37a9dab3dd2d387c6c11a90eb17f97
qiusheng1 <qiusheng1@lenovo.com>
2019-07-05 10:16:41 +0800

    体育

--- photo-gallery/src/app/app-routing.module.ts
+++ photo-gallery/src/app/app-routing.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {CheckTutorial} from './providers/check-tutorial.service';
 
 const routes: Routes = [
   {
     path: 'tutorial',
-    loadChildren: './pages/tutorial/tutorial.module#TutorialPageModule'
+    loadChildren: './pages/tutorial/tutorial.module#TutorialPageModule',
+    canLoad: [CheckTutorial]
   },
   {
     path: '',
     redirectTo: 'tutorial',
     pathMatch: 'full'
-  }
+  },
+  { path: 'app', loadChildren: './pages/tabs-page/tabs-page.module#TabsPagePageModule' },
+  { path: 'speaker-list', loadChildren: './pages/speaker-list/speaker-list.module#SpeakerListPageModule' },
+  { path: 'speaker-detail', loadChildren: './pages/speaker-detail/speaker-detail.module#SpeakerDetailPageModule' },
+  { path: 'map', loadChildren: './pages/map/map.module#MapPageModule' },
+  { path: 'about', loadChildren: './pages/about/about.module#AboutPageModule' },
+  { path: 'session-detail', loadChildren: './pages/session-detail/session-detail.module#SessionDetailPageModule' },
 ];
 @NgModule({
   imports: [
